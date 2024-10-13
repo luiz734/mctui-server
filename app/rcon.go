a4fa8956ed3e22848ad9fcd2a9f655a23bc2d9af
Luiz G. <luiz007@live.com>
2024-10-13 00:15:38 -0300

    Add https

--- app/rcon.go
+++ app/rcon.go
@@ -1,21 +1,22 @@
-package app;
+package app
 
 import (
-	"log"
+	"fmt"
+
 	"github.com/gorcon/rcon"
 )
 
 func AskRconServer(command string) string {
 	conn, err := rcon.Dial("127.0.0.1:25575", "minecraft")
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Sprintf("%s\n%s", err.Error(), "Is the server down?")
 	}
 	defer conn.Close()
 
 	response, err := conn.Execute(command)
 	if err != nil {
-		log.Fatal(err)
+		return fmt.Sprintf("%s", err.Error())
 	}
-	
-    return response
+
+	return response
 }
--- main.go
+++ main.go
@@ -1,34 +1,64 @@
 package main
 
 import (
+	"crypto/tls"
 	"encoding/json"
 	"fmt"
 	"log"
 	"mctui-server/app"
 	"net/http"
+	"strings"
 )
 
 type Command struct {
-    Command string `json:"command"`
+	Command string `json:"command"`
 }
-func commandHandler(w http.ResponseWriter, r *http.Request) {
-    var cmd Command
-    // Decode the JSON body
-    err := json.NewDecoder(r.Body).Decode(&cmd)
-    if err != nil {
-        http.Error(w, "Bad request", http.StatusBadRequest)
-        return
-    }
 
-    // Log or process the command
-    fmt.Fprintf(w, app.AskRconServer(cmd.Command))
+var (
+	CertFilePath = "cert/cert.pem"
+	KeyFilePath  = "cert/key.pem"
+)
+
+func commandHandler(w http.ResponseWriter, r *http.Request) {
+	var cmd Command
+	// Decode the JSON body
+	err := json.NewDecoder(r.Body).Decode(&cmd)
+	if err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	withoutPrefix, found := strings.CutPrefix(cmd.Command, "!")
+	if found {
+		fmt.Fprintf(w, fmt.Sprintf("%s not implemented yet...", withoutPrefix))
+		return
+	}
+	fmt.Fprintf(w, app.AskRconServer(cmd.Command))
 }
 
 func main() {
-    http.HandleFunc("/command", commandHandler)
+	serverTLSCert, err := tls.LoadX509KeyPair(CertFilePath, KeyFilePath)
+	if err != nil {
+		log.Fatalf("Error loading certificate and key file: %v", err)
+	}
+	tlsConfig := &tls.Config{
+		Certificates: []tls.Certificate{serverTLSCert},
+	}
 
-    port := 8090
-    addr := fmt.Sprintf(":%d", port)
-    log.Printf("Listen on port %d", port)
-    http.ListenAndServe(addr, nil)
+	port := 8090
+	addr := fmt.Sprintf(":%d", port)
+
+	mux := http.NewServeMux()
+	mux.HandleFunc("/command", commandHandler)
+
+	server := http.Server{
+		Addr:      addr,
+		Handler:   mux,
+		TLSConfig: tlsConfig,
+	}
+	defer server.Close()
+
+	log.Printf("Listen on port %d", port)
+	server.ListenAndServeTLS("", "")
+	http.ListenAndServe(addr, nil)
 }
